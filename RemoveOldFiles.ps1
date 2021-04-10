940d4d0c4f800a0827d045d7d87080aef35851e4
anneposo <43505612+anneposo@users.noreply.github.com>
2021-04-09 18:13:56 -0700

    File directory cleanup

---
+++ RemoveOldFiles.ps1
@@ -0,0 +1,3 @@
+﻿# Deletes files and folers recursively that were last modified over 7 days ago
+
+Get-ChildItem 'E:\RM' | Where-Object {$_.LastWriteTime -le (Get-Date).AddDays(-7)} | Foreach-Object { Remove-Item $_.FullName -Recurse -Force} 
