dde0a08115be9b316e6b2b9f8538dc3b72b97e85
anneposo <43505612+anneposo@users.noreply.github.com>
2021-12-28 21:20:12 -0800

    VSTS get repository info list

---
+++ Azure DevOps/VSTS_Report_GitURL.ps1
@@ -0,0 +1,23 @@
+﻿# Generates report of an organization's Azure DevOps repository names and the repo URL
+
+$Account ="AzureDevOpsAccountName"
+$Accesstoken=" "
+$passkey = ":$($Accesstoken)"
+
+$encodedKey = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($passkey))
+$token = "Basic $encodedKey"
+
+$list = @()
+$url = "https://$Account.visualstudio.com/_apis/git/repositories?api-version=5.1"
+$result = Invoke-RestMethod $url -Method Get -Headers @{ Authorization = $token }
+    
+if ($result.count -gt 0){
+    $result.value | % {
+        $list += [pscustomobject]@{
+            Project = $_.project.name
+            url = $_.remoteUrl
+        }
+    }
+}
+
+$list| Sort-Object -Property Project |Export-Csv -Path "C:\FilePath\giturl.csv" -NoTypeInformation
---
+++ Azure DevOps/VSTS_Report_userlist.ps1
@@ -0,0 +1,34 @@
+﻿# Generates report of organization's repository user access list in Azure DevOps 
+
+$Account ="AzureDevOpsAccountName"
+$Accesstoken=" "
+
+$passkey = ":$($Accesstoken)"
+
+$encodedKey = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($passkey))
+$token = "Basic $encodedKey"
+
+$projectsResult = Invoke-RestMethod "https://$Account.visualstudio.com/_apis/projects?`$top=500&api-version=5.1" -Method Get -Headers @{ Authorization = $token }
+
+$list = @()
+
+$projectsResult.value | % {
+    $project = $_.name
+    $projectID = $_.id
+
+    $url = "https://$Account.visualstudio.com/_apis/projects/$projectID/teams/$project Team/members?api-version=5.1"
+
+    $result = Invoke-RestMethod $url -Method Get -Headers @{ Authorization = $token }
+
+    if ($result.count -gt 0){
+        $result.value | % {
+            $list += [pscustomobject]@{
+                Project = $project
+                member=$_.identity.displayName
+                email=$_.identity.uniqueName
+            }
+        }
+    }
+}
+
+$list| Sort-Object -Property Project|Export-Csv -Path "C:\FilePath\userlist.csv" -NoTypeInformation
